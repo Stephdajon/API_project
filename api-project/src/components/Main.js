58e95c4e23c88f4bd7ea944ee2d8ea62fc5c624f
stephdajon <stephdajon@gmail.com>
2021-07-30 19:10:24 +0800

    mini api-project done

--- api-project/src/components/Main.js
+++ api-project/src/components/Main.js
@@ -1,21 +1,19 @@
-import Coin from '../coins/Coins'
+import './Main.scss'
+import Coin from './coins/Coins'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const PageTwo = () => {
+const Main = () => {
     //useState to get all the coins
     const [coins, setCoins] = useState([]);
     //useState for search input
     const [search, setSearch] = useState('');
-    //useState for pages
-    // const [counter, setCounter] = useState(2);
 
     useEffect(() => {
-        axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=2&sparkline=true&price_change_percentage=1h%2C24%2C7d
-        `)
+      const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=near%2Cdecentraland%2Czcash%2Cblockstack%2Ctelcoin%2Cyearn-finance%2Chelium%2Cenjincoin%2Cholotoken%2Chavven%2Cflow%2Csmooth-love-potion%2Cankr%2Cnano%2Cicon%2C0x%2Cbitcoin-gold%2Cbasic-attention-token%2Csiacoin%2Cdigibyte%2Cfantom%2Ctron%2Chedera-hashgraph%2Cmonero%2Cbittorrent-2%2Cpancakeswap-token%2Cshiba-inu%2Cethlend%2Ccompound-ether%2Cftx-token%2Cvechain%2Cterra-luna%2Cstellar%2Ctheta-token%2Ccryptoblades%2Cmatic-network%2Cethereum-classic%2Csolana%2Cchainlink%2Cwrapped-bitcoin%2Caxie-infinity%2Cuniswap%2Clitecoin%2Cpolkadot%2Cbitcoin-cash%2Cbitcoin%2Cetherium%2Cbinancecoin%2Ccardano%2Cripple%2Cdogecoin%2C&order=market_cap_desc&per_page=250&page=1&sparkline=false"
+        axios.get(url)
             .then(res => {
               setCoins(res.data);
-            //   setCounter(counter++)
               console.log(res.data);
             })
             .catch(err => 
@@ -44,6 +42,8 @@ const PageTwo = () => {
             />
           </form>
         </div>
+
+        <div className="coin-main">
         {filteredCoins.map(coin => {
           return (
             <Coin
@@ -58,7 +58,8 @@ const PageTwo = () => {
             />
           );
         })}
+        </div>
       </div>
     );
   }
-export default PageTwo;
+export default Main;
